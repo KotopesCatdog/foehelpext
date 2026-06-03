00a60f8413d462feb645dc5d7f4219a3b598f2df
KotopesCatdog <ceresit@gmail.com>
2026-06-03 17:27:20 +0300

    del

--- modules/bubble/bubble.js
+++
@@ -1,8 +0,0 @@
-const BUBBLE_GAME_DATA = {};
-const COLORS = ['#FF6B6B', '#4ECDC4', '#45B7D1', '#FFA07A', '#98D8C8', '#F7DC6F', '#BB8FCE'];
-const PARTICLE_COUNT = 8;
-const BUBBLE_SIZE = 24;
-
-function randColor() { return Math.floor(Math.random() * COLORS.length); }
-
-// ... [Game logic code continues - full bubble.js content]
